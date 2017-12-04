be33f57519354814707784f50583aa3451e87bb1
someone <someone@someones-MacBook-Pro.local>
2017-12-03 22:12:58 -0500

    added test route

--- routes/test.js
+++ routes/test.js
@@ -5,7 +5,14 @@ const router = express.Router();
 const { ensureAuth } = require("../helpers/auth");
 
 router.get("/form", ensureAuth, (req,res) => {
-    res.send("hey i'm the form test page.");
+    res.render("test/test");
+})
+
+router.post("/form", ensureAuth, (req,res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const confirmationForm = `${lastName}, ${firstName}`;
+    res.render("test/test",{confirmationForm:confirmationForm});
 })
 
 module.exports = router;
